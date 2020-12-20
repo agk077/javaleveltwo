6c6587ac0480c890824122f5398fc6a12f2a1ea4
tango! <agk077@ya.ru>
2020-12-21 02:16:50 +0300

    Домашнее задание, урок 7, Java уровень 2

---
+++ src/ru/geekbrain/agk077/javaleveltwo/lesson07/chat/ChatApp.java
@@ -0,0 +1,11 @@
+package ru.geekbrain.agk077.javaleveltwo.lesson07.chat;
+/*
+
+ДЗ урок 7 Java уровень 2 by agk077@ya.ru (tango!)
+21.12.20
+ */
+public class ChatApp {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
